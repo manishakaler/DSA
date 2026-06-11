4169b8aa79d4c0ce99c54b299c391256e6f7b593
Manisha Kaler <49517860+manishakaler@users.noreply.github.com>
2026-06-11 18:56:46 -0400

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int idxA, idxB = 0;
+        unordered_map<int,int>elemIdxMap;
+        int n = nums.size();
+
+        for (int i = 0; i < n; ++i){
+            elemIdxMap[nums[i]]= i;
+           
+            
+        }
+
+         for (int i = 0; i < n; ++i){
+           int diff = target - nums[i];
+
+            if(elemIdxMap.count(diff) && elemIdxMap[diff] != i ){
+                return {i, elemIdxMap[target - nums[i]]};
+            }
+           
+            
+        }
+
+         
+      
+        return {};
+        // unordered_map<int, int> indices;
+        // for (int i = 0; i < nums.size(); i++) {
+        //     indices[nums[i]] = i;
+        // }
+
+        // for (int i = 0; i < nums.size(); i++) {
+        //     int diff = target - nums[i];
+        //     if (indices.count(diff) && indices[diff] != i) {
+        //         return {i, indices[diff]};
+        //     }
+        // }
+
+        // return {};
+    }
+};
